1ffa71c2fb8634eed13950b4dab0138b04606ddf
dqwcppfans <dqwcppfans@foxmail.com>
2016-03-25 22:31:42 +0800

    eval simple lang

--- src/eval.kt
+++ src/eval.kt
@@ -8,62 +8,80 @@ private var lang = """
 // start
 a = 1
 b = 2
-c = 3
+c = a
 print(c)
 print(a)
 print(b)
 print(123)
 """
 
-class Env
+class Env(private val cache: HashMap<String, Int> = HashMap()) {
 
-interface Visitor {
-    fun visit(env: Env, exp: Stat)
-    fun visit(env: Env, exp: Assign)
-    fun visit(env: Env, exp: Expr)
-    fun visit(env: Env, exp: Expr.IntExpr)
-    fun visit(env: Env, exp: Expr.SymbolExpr)
-    fun visit(env: Env, exp: FuncCall)
+    fun store(id: String, value: Int) {
+        cache.put(id, value)
+    }
+
+    fun fetch(id: String) = cache.get(id)
 }
 
-class EvalVisitor: Visitor {
+interface Visitor<T> {
+    fun visit(env: Env, exp: Stat): T
+    fun visit(env: Env, exp: Assign): T
+    fun visit(env: Env, exp: Expr): T
+    fun visit(env: Env, exp: Expr.IntExpr): T
+    fun visit(env: Env, exp: Expr.SymbolExpr): T
+    fun visit(env: Env, exp: FuncCall): T
+}
 
-    override fun visit(env: Env, exp: Stat) {
-        throw UnsupportedOperationException()
+class EvalVisitor : Visitor<Int> {
+
+    override fun visit(env: Env, exp: Stat): Int {
+        return when (exp) {
+            is Assign -> visit(env, exp)
+            is FuncCall -> visit(env, exp)
+            else -> throw RuntimeException()
+        }
     }
 
-    override fun visit(env: Env, exp: Assign) {
-        throw UnsupportedOperationException()
+    override fun visit(env: Env, exp: Assign): Int {
+        val v = visit(env, exp.value)
+        env.store(exp.id, v)
+        return v
     }
 
-    override fun visit(env: Env, exp: Expr) {
-        throw UnsupportedOperationException()
+    override fun visit(env: Env, exp: Expr): Int {
+        return when (exp) {
+            is Expr.IntExpr -> visit(env, exp)
+            is Expr.SymbolExpr -> visit(env, exp)
+        }
     }
 
-    override fun visit(env: Env, exp: Expr.IntExpr) {
-        throw UnsupportedOperationException()
+    override fun visit(env: Env, exp: Expr.IntExpr): Int {
+        return exp.value
     }
 
-    override fun visit(env: Env, exp: Expr.SymbolExpr) {
-        throw UnsupportedOperationException()
+    override fun visit(env: Env, exp: Expr.SymbolExpr): Int {
+        return env.fetch(exp.id) ?: throw RuntimeException()
     }
 
-    override fun visit(env: Env, exp: FuncCall) {
-        throw UnsupportedOperationException()
+    override fun visit(env: Env, exp: FuncCall): Int {
+        var v = visit(env, exp.param)
+        println("--- $v ---")
+        return v
     }
 
 }
 
 // statement
 abstract class Stat {
-    open fun apply(env: Env, visitor: Visitor) {
+    open fun<T> apply(env: Env, visitor: Visitor<T>) {
         visitor.visit(env, this)
     }
 }
 
 // assignment
 class Assign(val id: String, val value: Expr) : Stat() {
-    override fun apply(env: Env, visitor: Visitor) {
+    override fun<T> apply(env: Env, visitor: Visitor<T>) {
         visitor.visit(env, this)
     }
 }
@@ -71,25 +89,25 @@ class Assign(val id: String, val value: Expr) : Stat() {
 // expression
 sealed class Expr : Stat() {
     class IntExpr(val value: Int) : Expr() {
-        override fun apply(env: Env, visitor: Visitor) {
+        override fun<T> apply(env: Env, visitor: Visitor<T>) {
             visitor.visit(env, this)
         }
     }
 
     class SymbolExpr(val id: String) : Expr() {
-        override fun apply(env: Env, visitor: Visitor) {
+        override fun<T> apply(env: Env, visitor: Visitor<T>) {
             visitor.visit(env, this)
         }
     }
 
-    override fun apply(env: Env, visitor: Visitor) {
+    override fun<T> apply(env: Env, visitor: Visitor<T>) {
         visitor.visit(env, this)
     }
 }
 
 // function call
-class FuncCall(val name: String, vararg params: Expr) : Stat() {
-    override fun apply(env: Env, visitor: Visitor) {
+class FuncCall(val name: String, val param: Expr) : Stat() {
+    override fun<T> apply(env: Env, visitor: Visitor<T>) {
         visitor.visit(env, this)
     }
 }
@@ -100,8 +118,14 @@ fun main(args: Array<String>) {
     var stats = ArrayList<Stat>()
     lines.forEach { line ->
         if (line.contains("=")) {
-            var (id, expr) = line.split("\\s*=\\s*".toRegex())
-            stats.add(Assign(id, Expr.IntExpr(expr.toInt())))
+            var (id, exprStr) = line.split("\\s*=\\s*".toRegex())
+            val expr: Expr
+            if (exprStr.matches("\\d+".toRegex())) {
+                expr = Expr.IntExpr(exprStr.toInt())
+            } else {
+                expr = Expr.SymbolExpr(exprStr)
+            }
+            stats.add(Assign(id, expr))
         } else if (line.startsWith("print")) {
             var exprStr = line.substringAfter("print(").substringBeforeLast(')')
             val expr: Expr
